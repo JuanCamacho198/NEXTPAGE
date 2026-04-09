3da0bcce5374f86e629334b4239e44ff4b5b7ee2
Juan_Camacho <jcamachomolina503@gmail.com>
2026-04-08 23:05:12 -0500

    Pass AppContainer to NextPageNavHost in MainActivity for improved dependency injection

--- android/app/src/main/java/com/nextpage/MainActivity.kt
+++ android/app/src/main/java/com/nextpage/MainActivity.kt
@@ -3,15 +3,17 @@ package com.nextpage
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.nextpage.di.AppContainer
 import com.nextpage.presentation.navigation.NextPageNavHost
 import com.nextpage.presentation.theme.NextPageTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val appContainer = AppContainer(context = this)
         setContent {
             NextPageTheme {
-                NextPageNavHost()
+                NextPageNavHost(appContainer = appContainer)
             }
         }
     }

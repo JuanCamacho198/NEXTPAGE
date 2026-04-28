ca6973094cdb60912c57044c2bd763c50d4226ce
Juan_Camacho <jcamachomolina503@gmail.com>
2026-04-27 23:22:24 -0500

    test: configure Bun test environment with happy-dom and global shims

--- desktop/src/test/setup.ts
+++ desktop/src/test/setup.ts
@@ -1,1 +1,23 @@
 import "@testing-library/jest-dom/vitest";
+import { Window } from "happy-dom";
+
+const window = new Window();
+// @ts-ignore
+global.window = window;
+// @ts-ignore
+global.document = window.document;
+// @ts-ignore
+global.Node = window.Node;
+// @ts-ignore
+global.Element = window.Element;
+// @ts-ignore
+global.HTMLElement = window.HTMLElement;
+// @ts-ignore
+global.KeyboardEvent = window.KeyboardEvent;
+// @ts-ignore
+global.localStorage = window.localStorage;
+// @ts-ignore
+global.sessionStorage = window.sessionStorage;
+// @ts-ignore
+global.navigator = window.navigator;
+

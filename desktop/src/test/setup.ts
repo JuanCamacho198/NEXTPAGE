c5f718a61f5358883c65f17ff5ef7789bcbe4a10
Juan_Camacho <jcamachomolina503@gmail.com>
2026-05-04 10:49:22 -0500

    refactor: clean up setup.ts by removing unused global window setup code

--- desktop/src/test/setup.ts
+++ desktop/src/test/setup.ts
@@ -1,23 +1,2 @@
-import "@testing-library/jest-dom/vitest";
-import { Window } from "happy-dom";
-
-const window = new Window();
-// @ts-ignore
-global.window = window;
-// @ts-ignore
-global.document = window.document;
-// @ts-ignore
-global.Node = window.Node;
-// @ts-ignore
-global.Element = window.Element;
-// @ts-ignore
-global.HTMLElement = window.HTMLElement;
-// @ts-ignore
-global.KeyboardEvent = window.KeyboardEvent;
-// @ts-ignore
-global.localStorage = window.localStorage;
-// @ts-ignore
-global.sessionStorage = window.sessionStorage;
-// @ts-ignore
-global.navigator = window.navigator;
-
+// Setup file for vitest
+// Note: happy-dom is loaded automatically via environment: "happy-dom"

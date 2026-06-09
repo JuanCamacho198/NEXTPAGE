9392e4159656a65b58aceb34bcd50e58c50a0b26
Juan_Camacho <jcamachomolina503@gmail.com>
2026-06-09 12:10:40 -0500

    refactor: remove in-app language switcher from MainActivity
    Removed AppLanguagePreferences initialization and locale setup
    from MainActivity.onCreate. The language switching is now handled
    entirely via string resources and system locale.

--- android/app/src/main/java/com/nextpage/MainActivity.kt
+++ android/app/src/main/java/com/nextpage/MainActivity.kt
@@ -4,14 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.core.os.LocaleListCompat
-import com.nextpage.data.session.AppLanguagePreferences
 import com.nextpage.data.session.AppThemePreferences
 import com.nextpage.di.AppContainer
 import com.nextpage.domain.model.ThemeMode
@@ -22,13 +19,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appContainer: AppContainer
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        val langPrefs = rememberLanguagePrefs()
-        val langCode = langPrefs.load()
-        if (langCode != null) {
-            AppCompatDelegate.setApplicationLocales(
-                LocaleListCompat.forLanguageTags(langCode)
-            )
-        }
         super.onCreate(savedInstanceState)
         appContainer = AppContainer(context = this)
         handleAuthIntent(intent)
@@ -53,10 +43,6 @@ class MainActivity : AppCompatActivity() {
                 )
             }
         }
-    }
-
-    private fun rememberLanguagePrefs(): AppLanguagePreferences {
-        return AppLanguagePreferences(this)
     }
 
     override fun onNewIntent(intent: Intent) {

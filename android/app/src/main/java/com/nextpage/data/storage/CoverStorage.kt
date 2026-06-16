aa46a5652a316c1c7e236bd73a59ff1db48385b8
Juan_Camacho <jcamachomolina503@gmail.com>
2026-06-16 10:35:09 -0500

    feat(storage): add deleteCover() to CoverStorage interface
    Add deleteCover(bookId) method to enable cover file cleanup when
    books are deleted. Part of readium-post-migration-enhancements
    Phase 1 (foundation).

--- android/app/src/main/java/com/nextpage/data/storage/CoverStorage.kt
+++ android/app/src/main/java/com/nextpage/data/storage/CoverStorage.kt
@@ -2,4 +2,5 @@ package com.nextpage.data.storage
 
 interface CoverStorage {
     suspend fun saveCover(bookId: String, coverBytes: ByteArray): Result<String>
+    suspend fun deleteCover(bookId: String): Result<Unit>
 }
